b1fb031f3bfc75f030a6d0cedf3d35d3a1590e2b
Mareno Demmers <mhjdemmers@gmail.com>
2023-10-26 02:55:25 +0200

    AppliancesController added

---
+++ EnergieBewustLeven.API/Controllers/AppliancesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergieBewustLeven.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppliancesController : ControllerBase
+    {
+        // GET: 
+        [HttpGet]
+        public IActionResult GetAllAppliances()
+        {
+            string[] appliancesNames = new string[] { "yeet", "Jane", "Mark"};
+            return Ok();
+        }
+    }
+}
