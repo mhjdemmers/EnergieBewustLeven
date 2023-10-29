694e44ef52927aac8db69855efcd0fbc22336406
Mareno Demmers <mhjdemmers@gmail.com>
2023-10-29 15:17:50 +0100

    added DTOs

--- EnergieBewustLeven.API/Controllers/AppliancesController.cs
+++ EnergieBewustLeven.API/Controllers/AppliancesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using EnergieBewustLeven.API.Models.Domain;
 using EnergieBewustLeven.API.Data;
+using EnergieBewustLeven.API.Models.DTO;
 
 namespace EnergieBewustLeven.API.Controllers
 {
@@ -15,13 +16,55 @@ namespace EnergieBewustLeven.API.Controllers
         {
             this.dbContext = dbContext;
         }
-        // GET: 
+
+        // GET ALL APPLIANCES
+        //GET: 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var appliances = dbContext.Appliances.ToList();
+            // Get from database - Domain Model
+            var appliancesDomain = dbContext.Appliances.ToList();
 
-            return Ok(appliances);
+            // Map Domain Models to DTOs
+            var appliancesDTO = new List<ApplianceDTO>();
+            foreach (var applianceDomain in appliancesDomain)
+            {
+                appliancesDTO.Add(new ApplianceDTO()
+                {
+                    Id = applianceDomain.Id,
+                    Name = applianceDomain.Name,
+                    Brand = applianceDomain.Brand
+                });
+            }
+
+            // Return DTOs
+            return Ok(appliancesDTO);
+        }
+
+        //GET APPLIANCE BY ID
+        //GET:
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult Get([FromRoute] Guid id)
+        {
+            // Get from database - Domain Model
+            var applianceDomain = dbContext.Appliances.FirstOrDefault(x => x.Id == id);
+
+            if (applianceDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Map Domain Model to DTO
+            var applianceDTO = new ApplianceDTO()
+            {
+                Id = applianceDomain.Id,
+                Name = applianceDomain.Name,
+                Brand = applianceDomain.Brand
+            };
+
+            // Return DTO
+            return Ok(applianceDTO);
         }
     }
 }
---
+++ EnergieBewustLeven.API/Models/DTO/ApplianceDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EnergieBewustLeven.API.Models.DTO
+{
+    public class ApplianceDTO
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+    }
+}
