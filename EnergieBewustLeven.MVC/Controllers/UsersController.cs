96e2a3ec883c67fa923dbe6c07a2c977e3e351a5
Mareno Demmers <91221126+mhjdemmers@users.noreply.github.com>
2023-11-08 14:16:34 +0100

    add identity

---
+++ EnergieBewustLeven.MVC/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using EnergieBewustLeven.API.Data;
+using EnergieBewustLeven.MVC.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergieBewustLeven.MVC.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext dbContext;
+
+        public UsersController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            this.dbContext = dbContext;
+        }
+
+        public int GetLevelUser(ApplicationUser user)
+        {
+            int level = user.Level;
+            return level;
+        }
+
+        public IActionResult AddLevel(ApplicationUser user)
+        {
+            user.Level += 1;
+            return Ok(user);
+        }
+
+    }
+}
--- EnergieBewustLeven.MVC/Data/Migrations/20231108121741_changedIdentity.Designer.cs
+++ EnergieBewustLeven.MVC/Data/Migrations/20231108121741_changedIdentity.Designer.cs
@@ -4,6 +4,7 @@ using EnergieBewustLeven.MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EnergieBewustLeven.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231108121741_changedIdentity")]
+    partial class changedIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
